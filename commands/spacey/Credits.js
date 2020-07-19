ffcbcaf465ea9733cec721dc7f20700ad8523c10
Fraser McCallum <fraser@themccallums.net.nz>
2020-07-19 15:45:08 +1200

    Changed credits to use new client code

--- commands/spacey/Credits.js
+++ commands/spacey/Credits.js
@@ -1,43 +1,43 @@
 const { Command, CommandoMessage } = require("discord.js-commando");
 const { MessageEmbed, Message, GuildMember } = require("discord.js");
 
-const { PlayerModel } = require("space-y");
+const { Fetch } = require("spacey-client");
 const Util = require("../../Util.js");
 
 module.exports = class CreditsCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: "credits",
-			aliases: ["credit", "spacecredit", "cred", "%c"],
-			memberName: "credits",
-			group: "spacey",
-			description: "View your current number of credits in SpaceY.",
-			guildOnly: true,
-			clientPermissions: ["SEND_MESSAGES"],
-			args: [
-				{
-					key: "user",
-					prompt: "The user who's credits you would like to see",
-					type: "member",
-					default: (m) => m.member,
-				},
-			],
-		});
-	}
-	/**
-	 * @param {CommandoMessage} msg
-	 * @param {object} param1
-	 * @param {GuildMember} param1.user
-	 */
-	async run(msg, { user }) {
-		const discordId = user.id;
-		const player = await PlayerModel.findOneOrCreate({ uId: discordId });
+    constructor(client) {
+        super(client, {
+            name: "credits",
+            aliases: ["credit", "spacecredit", "cred", "%c"],
+            memberName: "credits",
+            group: "spacey",
+            description: "View your current number of credits in SpaceY.",
+            guildOnly: true,
+            clientPermissions: ["SEND_MESSAGES"],
+            args: [
+                {
+                    key: "user",
+                    prompt: "The user who's credits you would like to see",
+                    type: "member",
+                    default: (m) => m.member,
+                },
+            ],
+        });
+    }
+    /**
+     * @param {CommandoMessage} msg
+     * @param {object} param1
+     * @param {GuildMember} param1.user
+     */
+    async run(msg, { user }) {
+        const discordId = user.id;
+        const client = await Fetch(discordId);
 
-		const output = new MessageEmbed()
-			.setTitle(`${user.nickname || user.displayName}'s Credits`)
-			.setDescription(`${Util.delimit(player.Credits)}¢`)
-			.setColor("#24c718")
-			.setThumbnail(user.user.displayAvatarURL());
-		return msg.say(output);
-	}
+        const output = new MessageEmbed()
+            .setTitle(`${user.nickname || user.displayName}'s Credits`)
+            .setDescription(`${Util.delimit(client.Player.inventory.credits)}¢`)
+            .setColor("#24c718")
+            .setThumbnail(user.user.displayAvatarURL());
+        return msg.say(output);
+    }
 };
